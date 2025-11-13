63892b61bf9cc34b4bbc3c81e44123b0d2c4d38d
StraToTiQ <artur0griban123@gmail.com>
2025-11-13 09:12:04 +0100

    add player and drawing it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from logger import log_state
-from constants import *
+from constants import SCREEN_HEIGHT
+from constants import SCREEN_WIDTH
+from player import Player
 
 
 def main():
@@ -13,12 +15,17 @@ def main():
     timeClock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
         dt = timeClock.tick(60) / 1000
 
