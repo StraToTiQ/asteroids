fd1c5eade1b3927f7fa526f58f15ef9059c526f8
StraToTiQ <artur0griban123@gmail.com>
2025-11-13 08:48:39 +0100

    add fps measure

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    timeClock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = timeClock.tick(60) / 1000
 
 
 if __name__ == "__main__":
